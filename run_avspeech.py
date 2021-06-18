b40316a1c5c651959a4215941b9e80ce5ccb39f5
FrischJulien <jfrisch59@gmail.com>
2021-06-18 16:34:57 +0200

    Update run_avspeech.py

--- run_avspeech.py
+++ run_avspeech.py
@@ -1,11 +1,16 @@
 import os
+import subprocess
+
 
 #make sure the file exists
 os.system('sudo touch /mnt/efs/fs1/avspeech/output.txt')
 os.system('sudo chmod 777 /mnt/efs/fs1/avspeech/output.txt')
 
 #run the avspeech script
-data_path = "/mnt/efs/fs1/avspeech/raw/xao/"
+data_path = "/mnt/efs/fs1/avspeech/raw/"
+repositories=["xap","xaq","xar","xas"]
+
+
 tmp_dir="./temp/"
 syncnet_model="./data/syncnet_v2.model"
 min_track=10
@@ -15,15 +20,13 @@ sample_duration=1
 parts_num=8
 output_path="/mnt/efs/fs1/avspeech/output.txt"
 
-subcommande = ""
-for p in range(parts_num):
-    part=p
-    subcommande+="CUDA_VISIBLE_DEVICES={} python3 analyze_full_paralle2.py --data_path {} --tmp_dir {} --syncnet_model {} --min_track {} --min_face_size {} --batch_size {} --sample_duration {} --parts_num {} --part {} --output_path {}".format(part,data_path,tmp_dir,syncnet_model,min_track,min_face_size,batch_size,sample_duration,parts_num,part,output_path)
-    if p<parts_num-1:
-        subcommande+=" & "
-print("Launching for folder : {}\n".format(data_path))
-        
-#!python analyze_full_paralle2.py --data_path $data_path --tmp_dir $tmp_dir --syncnet_model $syncnet_model --min_track $min_track --min_face_size $min_face_size --batch_size $batch_size --ngpu $ngpu --sample_duration $sample_duration --parts_num $parts_num --part $part
 
-import subprocess
-subprocess.run(subcommande, shell=True)
+for rep in tqdm(repositories):
+    print("Launching process for repository {}\n".format(rep))  
+    subcommande = ""
+    for p in range(parts_num):
+        part=p
+        subcommande+="CUDA_VISIBLE_DEVICES={} python3 analyze_full_paralle2.py --data_path {} --tmp_dir {} --syncnet_model {} --min_track {} --min_face_size {} --batch_size {} --sample_duration {} --parts_num {} --part {} --output_path {}".format(part,data_path+rep,tmp_dir,syncnet_model,min_track,min_face_size,batch_size,sample_duration,parts_num,part,output_path)
+        if p<parts_num-1:
+            subcommande+=" & "       
+    subprocess.run(subcommande, shell=True)
